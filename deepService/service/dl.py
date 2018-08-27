b25fa41d8b25455aa4534a995092c5e26cc14d29
root <root@test.novalocal>
2018-08-27 15:06:04 +0800

    init project

---
+++ deepService/service/dl.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.split(os.path.realpath(__file__))[0]+"/..")
+import tornado
+from tornado.web import Application
+from util.ApiConfiger import ApiConfig
+import logging
+
+class DisDeepService(object):
+    def __init__(self):
+        self.config = ApiConfig()
+
+    def start(self):
+        app = tornado.web.Application([
+            (r"v1/train", None)
+            ])
+        app.listen(self.config.getint("service", "port"))
+        logging.info("service start ...")
+        tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    service = DisDeepService()
+    service.start()

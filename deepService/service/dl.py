2a88bf80785b338472327ecfc01fdb56bc070732
root <root@tf-1.novalocal>
2018-12-14 10:25:15 +0800

    add cwd

--- deepService/service/dl.py
+++ deepService/service/dl.py
@@ -7,7 +7,9 @@ from tornado.web import Application
 from util.ApiConfiger import ApiConfig
 from handlers.train_handler import TrainHandler
 from handlers.serving_handler import ServingHandler
+from handlers.test_handler import TestHandler
 from handlers.upload_handler import UploadHandler
+from handlers.user_handler import UserHandler
 import logging
 from logging.config import fileConfig
 
@@ -16,19 +18,25 @@ class DisDeepService(object):
         self.config = ApiConfig()
 
     def start(self):
+        settings = {
+                "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E="
+                }
         app = tornado.web.Application([
             (r"/v1/train", TrainHandler),
             (r"/v1/serving", ServingHandler),
+            (r"/v1/test", TestHandler),
+            (r"/v1/user/([a-zA-Z0-9_-]+)", UserHandler),
             (r"/v1/upload/(.*)", UploadHandler)
-            ])
+            ], **settings)
         app.listen(self.config.getint("service", "port"))
         logging.info("service start ...")
         tornado.ioloop.IOLoop.current().start()
 
 if __name__ == '__main__':
     wp = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    if not os.path.exists(wp+'/logs'):
-        os.mkdir(wp+'/logs')
+    wd = os.getcwd()
+    if not os.path.exists(wd+'/logs'):
+        os.mkdir(wd+'/logs')
     fileConfig(wp+'/conf/logging.conf')
     logger = logging.getLogger()
     logger.info('start ...')
